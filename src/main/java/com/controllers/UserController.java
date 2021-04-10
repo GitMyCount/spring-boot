2bdc9059aa7b6345235783aeac97f4084f912c61
user <dvd.off@mail.ru>
2021-04-11 03:50:15 +0700

    Initial commit

---
+++ src/main/java/com/controllers/UserController.java
@@ -0,0 +1,28 @@
+package com.controllers;
+
+import com.entity.User;
+import com.repositories.UserRepository;
+import com.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/hello")
+    public String userPage(Model model, Principal principal) {
+        User user = userRepository.findByUsername(principal.getName());
+        model.addAttribute("user", user);
+        return "show";
+    }
+
+}
